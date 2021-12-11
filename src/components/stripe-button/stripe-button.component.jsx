6ac60ac6cb014fe535ca4660c18b9ffea35a48bf
Dewansh Patle <dewanshpatle@Dewanshs-MacBook-Air.local>
2021-12-11 16:41:27 +0530

    added stipe and initial app build

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51K5O5dSAA1DbagpfggrsOw6zCRNa2qdCMJFidnScG9ITGXvgq5fWfQ25DuHcqH6PlQe6uRNYIKgnRLJ7GxYXLBFT00LmAZ238t';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,19 +1,42 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-
-const config =  {
-    apiKey: "AIzaSyDOeM9wXuw7gBeNm7-0sQi0XrLvFZzHURs",
-    authDomain: "crwn-db-2b0ce.firebaseapp.com",
-    projectId: "crwn-db-2b0ce",
-    storageBucket: "crwn-db-2b0ce.appspot.com",
-    messagingSenderId: "5903144229",
-    appId: "1:5903144229:web:a5578305e8e5dce08ef6cc",
-    measurementId: "G-MRFG95MMGW"
-  };
+const config = {
+  apiKey: "AIzaSyDOeM9wXuw7gBeNm7-0sQi0XrLvFZzHURs",
+  authDomain: "crwn-db-2b0ce.firebaseapp.com",
+  projectId: "crwn-db-2b0ce",
+  storageBucket: "crwn-db-2b0ce.appspot.com",
+  messagingSenderId: "5903144229",
+  appId: "1:5903144229:web:a5578305e8e5dce08ef6cc",
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
