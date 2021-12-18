b6a61b2df28e9dac23c17f08eaca7704e6ea9e5f
Dewansh Patle <dewanshpatle@Dewanshs-MacBook-Air.local>
2021-12-18 17:23:33 +0530

    shop data moved to firebase

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51K5O5dSAA1DbagpfggrsOw6zCRNa2qdCMJFidnScG9ITGXvgq5fWfQ25DuHcqH6PlQe6uRNYIKgnRLJ7GxYXLBFT00LmAZ238t';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
     console.log(token);
